9d010dd02e2a223db123a817065c19232c88a4d8
joctorok <141873911+joctorok@users.noreply.github.com>
2024-05-19 00:41:40 -0500

    Update Strum.gd

--- scripts/gameplay/Strum.gd
+++ scripts/gameplay/Strum.gd
@@ -8,8 +8,11 @@ func _ready():
 
 func strum_tween(time, type):
 	note.show()
+	note.modulate.a = 0
 	note.position.x = 152
 	note.frame = type - 1
+	var tween2 = get_tree().create_tween()
+	tween2.tween_property(note, "modulate:a", 1, 0.15)
 	var tween = get_tree().create_tween()
 	tween.tween_property(note, "position:x", -16, time)
 	tween.finished.connect(on_tween_finished)
